dce51656554a613e4d8f30af8bcc478598c3b58e
Franckoberg <franckoberg@gmail.com>
2022-03-21 11:07:12 -0400

    Test 1 2 3

--- store/models.py
+++ store/models.py
@@ -8,7 +8,7 @@ class Users(models.Model):
     email       = models.EmailField(max_length=245)
     username    = models.CharField(max_length=70)
     password    = models.CharField( max_length=50)
-    address		= models.CharField(max_length=150)
+    # address		= models.CharField(max_length=150)
     phone       = models.IntegerField()
     # pip install Pillow 
     foto        = models.ImageField(upload_to= 'userImage', default='image.png', height_field=None, width_field=None, max_length=100)
